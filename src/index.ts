796ab85b3ffb2aa91fc3269e1d72aef8e28e8876
Bruno Castro Garcia <bruneo32b@gmail.com>
2022-08-23 21:07:05 +0200

    Index base

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
+// import { exec } from "child_process";
+
+const VERSION = 0.1;
+const OS: string = process.platform.toLowerCase().includes("win") ? "win" : process.platform.toLowerCase().includes("linux") ? "linux" : process.platform.toLowerCase().includes("mac") ? "macos" : "unknown";
+
 export async function exec(argv2: string[]) {
 	console.log(argv2);
+
+	switch (argv2[0]) {
+		case "-h":
+		case "--help":
+			console.log("HELP");
+			break;
+
+		case "-v":
+		case "--version":
+			console.log(VERSION.toFixed(1));
+			break;
+
+		case "b":
+		case "build":
+			console.log(__dirname);
+
+			switch (OS) {
+				case "win":
+					break;
+
+				case "linux":
+					break;
+
+				default:
+					console.log("ERR! Unknown OS");
+					break;
+			}
+			break;
+
+		default:
+			console.log("Nothing to do. Type 'androidjs --help' to get more information");
+			break;
+	}
+
 }
